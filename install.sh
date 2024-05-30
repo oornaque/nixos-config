26d8a4134947e065c280d00f5ca8c8b2ff622628
Oriol <142458783+oornaque@users.noreply.github.com>
2024-05-30 23:11:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ cd nixos-config
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disk-config.nix
 # enter luks password
 sudo nixos-generate-config --no-filesystems --root /mnt
-cp *.nix /mnt/etc/nixos
+sudo cp *.nix /mnt/etc/nixos
 cd /mnt/etc/nixos
-nixos-install --flake .#main
+sudo nixos-install --flake .#main
+sudo nixos-rebuild switch
+reboot
