db1e8b76bf30abe7f590883b1bf528253cc9c936
Oriol <142458783+oornaque@users.noreply.github.com>
2024-05-30 23:19:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-nix-shell -p git
-git clone https://github.com/oornaque/nixos-config
-cd nixos-config
+#nix-shell -p git
+#git clone https://github.com/oornaque/nixos-config
+#cd nixos-config
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disk-config.nix
 # enter luks password
 sudo nixos-generate-config --no-filesystems --root /mnt
